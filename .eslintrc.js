d240397916087c244e44516bff4b16836863b412
Daniel Barnes <danielbarnes175@gmail.com>
2022-06-04 00:00:26 -0700

    Add linting

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": false
+        },
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+    ],
+    "rules": {
+    }
+}
