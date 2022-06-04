fc20becfd76b401223ba4236b92de3c98d3fc273
Daniel Barnes <danielbarnes175@gmail.com>
2022-06-03 23:07:58 -0700

    Track quest in journal

--- src/macros/journal/journal.js
+++ src/macros/journal/journal.js
@@ -7,3 +7,31 @@
     },
     init: function() { },
   };
+
+  macros['getQuestListInJournal'] = {
+    handler: function(place, macroName, params, parser) {
+        let activeQuests = [];
+        let completedQuests = [];
+        for (const [key, value] of Object.entries(State.variables.quests.questLog)) {
+            if (value.knownQuest && value.currentStep < value.questSteps.length - 1) {
+                activeQuests.push({
+                    name: value.title,
+                    step: value.questSteps[value.currentStep]
+                });
+            } else if (value.knownQuest) {
+                completedQuests.push(value.title)
+            }
+        }
+
+        new Wikifier(place, "''Active Quests''\n");
+        activeQuests.forEach(quest => {
+            new Wikifier(place, `${quest.name} - ${quest.step}\n`);
+        });
+
+        new Wikifier(place, "''Completed Quests''\n");
+        completedQuests.forEach(quest => {
+            new Wikifier(place, `${quest}\n`);
+        });
+    },
+    init: function() { },
+  }
