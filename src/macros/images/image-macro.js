47e299bf51f2f859e78854b60b2317a74f8664ef
Daniel Barnes <danielbarnes175@gmail.com>
2022-06-06 15:11:10 -0700

    Lint

--- src/macros/images/image-macro.js
+++ src/macros/images/image-macro.js
@@ -1,40 +1,37 @@
 macros.getBannerForLocation = {
-    /* eslint-disable-next-line */
+  /* eslint-disable-next-line */
     handler(place, macroName, params, parser) {
-      const imageKey = params[0].toLowerCase().replace(' ', '-');
-      const directory = (ENV === 'production') ? 'src/images/' : '../src/images/';
+    const imageKey = params[0].toLowerCase().replace(' ', '-');
+    const directory = (ENV === 'production') ? 'src/images/' : '../src/images/';
 
-      let time = '';
-      if (params[1]) {
-        if (currentHour < 12 && currentHour >= 6) {
-            time = '-morning';
-        }
-        else if (currentHour < 18) {
-            time = '-afternoon';
-        }
-        else if (currentHour < 22) {
-            time = '-evening';
-        }
-        else {
-            time = '-night';
-        }
+    let time = '';
+    if (params[1]) {
+      if (currentHour < 12 && currentHour >= 6) {
+        time = '-morning';
+      } else if (currentHour < 18) {
+        time = '-afternoon';
+      } else if (currentHour < 22) {
+        time = '-evening';
+      } else {
+        time = '-night';
       }
+    }
 
-      const fileType = '.png';
-      new Wikifier(place, `<img src="${directory}${imageKey}${time}${fileType}" width=850 height=300>`);
-    },
-    init() { },
-  };
+    const fileType = '.png';
+    new Wikifier(place, `<img src="${directory}${imageKey}${time}${fileType}" width=850 height=300>`);
+  },
+  init() { },
+};
 
 macros.getImage = {
-    /* eslint-disable-next-line */
+  /* eslint-disable-next-line */
     handler(place, macroName, params, parser) {
-        const imageKey = params[0];
-        const directory = (ENV === 'production') ? 'src/images/' : '../src/images/';
-        const width = params[1];
-        const height = params[2];
+    const imageKey = params[0];
+    const directory = (ENV === 'production') ? 'src/images/' : '../src/images/';
+    const width = params[1];
+    const height = params[2];
 
-        new Wikifier(place, `<img src="${directory}${imageKey}" width='${width}' height='${height}'>`);
-      },
-      init() { },
-}
+    new Wikifier(place, `<img src="${directory}${imageKey}" width='${width}' height='${height}'>`);
+  },
+  init() { },
+};
