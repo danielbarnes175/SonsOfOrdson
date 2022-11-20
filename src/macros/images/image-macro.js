8138df640cfe523292bdd97fb03adc18aa6443e4
Daniel Barnes <danielbarnes175@gmail.com>
2022-11-19 22:52:45 -0800

    Update background art

--- src/macros/images/image-macro.js
+++ src/macros/images/image-macro.js
@@ -1,7 +1,9 @@
 macros.getBannerForLocation = {
   /* eslint-disable-next-line */
     handler(place, macroName, params, parser) {
-    const imageKey = params[0].toLowerCase().replaceAll(' ', '-');
+    let imageKey = params[0].toLowerCase().replaceAll(' ', '-');
+    imageKey = imageKey.replace(/'/g, '');
+    console.log(imageKey);
     const directory = (ENV === 'production') ? 'src/images/' : '../src/images/';
 
     let time = '';
@@ -18,7 +20,7 @@ macros.getBannerForLocation = {
     }
 
     const fileType = '.png';
-    new Wikifier(place, `<img src="${directory}${imageKey}${time}${fileType}" onerror="this.onerror=null; this.src='${directory}default-banner.png'" alt="Image of ${State.variables.player.location}"  width=850 height=300>`);
+    new Wikifier(place, `<img src="${directory}${imageKey}${time}${fileType}" class="bannerImage" onerror="this.onerror=null; this.src='${directory}default-banner.png'" alt="Image of ${State.variables.player.location}">`);
   },
   init() { },
 };
